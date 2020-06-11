6a88917b01800ca8a9c5dc2bc818c922896155e7
kungfupandadab <kungfupandadab@users.noreply.github.com>
2020-06-11 05:25:36 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -4,23 +4,26 @@
 
 int main(void)
 {
+    //creates variable
     int height;
+
     do
-        {
-            height = get_int("Height: ");
-        }
-        while (height<1 || height>8);
-    for (int i = 1; i<height+1; i++)
     {
-        for(int b=height; b>i;b--)
+        //gets user input
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8); //conditional statements foe height requirements
+    for (int i = 1; i < height + 1; i++) //for every line...
+    {
+        for (int b = height; b > i; b--) //prints the spaces that come before the asterisk(s)
         {
             printf(" ");
         }
-        for(int a=1; a<i+1;a++)
+        for (int a = 1; a < i + 1; a++) //prints the asterisk(s)
         {
             printf("#");
         }
-        printf("\n");
+        printf("\n"); //new line
     }
 
 }
