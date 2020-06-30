55f1afc690e1f0270a7c479864b9a783ea78eabe
kungfupandadab <kungfupandadab@users.noreply.github.com>
2020-06-30 05:48:13 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -7,10 +7,11 @@ int main(void)
 {
     int j;
     string read;
-    int letters = 0;
+    float letters = 0;
     int words = 0;
-    int sentences = 0;
-    int index;
+    float sentences = 0;
+    float index = 0.0;
+    int rindex;
     do
     {
         read = get_string("Text:");
@@ -22,27 +23,47 @@ int main(void)
     {
         if((read[i]>='a' && read[i]<='z') || (read[i]>='A' && read[i]<='Z'))
         {
+            printf("Letter found in %d\n",i);
             letters++;
 
         }
         else if (read[i] == ' ')
         {
+            printf("Space found in %d\n",i);
             if((read[i+1]>='a' && read[i+1]<='z') || (read[i+1]>='A' && read[i+1]<='Z'))
                 words++;
         }
         else if (read[i] == '.' || read[i] == '!' || read[i] == '?')
         {
+            printf("Punctuation found in %d\n",i);
             sentences++;
         }
     }
     words++;
+
+    printf("%f\n",letters);
+    printf("%f\n",sentences);
+    printf("%d\n",words);
+
     letters = letters *100/words;
     sentences = sentences * 100/words;
+
+    printf("%f\n",letters);
+    printf("%f\n",sentences);
+
+    //letters = round(letters);
+    //sentences = round(sentences);
+
+    printf("%f\n",letters);
+    printf("%f\n",sentences);
+
+    rindex = (int)index;
+
     index = 0.0588 * letters - 0.296 * sentences - 15.8;
     index = round(index);
     if (index>=1 && index<=16)
     {
-        printf("Grade %d\n",index);
+        printf("Grade %f\n",index);
     }
     else if (index<1)
     {
