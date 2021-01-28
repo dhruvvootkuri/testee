59fe233bc7d0b8784ea973113ac8677c8bbce7b1
Dhruv Fallon <50392973+kungfupandadab@users.noreply.github.com>
2021-01-27 19:56:37 -0800

    Update plurality.c

--- plurality.c
+++ plurality.c
@@ -6,7 +6,7 @@
 #define MAX 9
 
 // Candidates have name and vote count
-typedef struct
+typedef struct //This is sort of like creating an object Candidate with attributes name and votes.
 {
     string name;
     int votes;
@@ -21,7 +21,7 @@ int candidate_count;
 
 // Function prototypes
 bool vote(string name);
-void print_winner(void);
+void print_winner(void); //This defines that is later created
 
 int main(int argc, string argv[])
 {
@@ -34,18 +34,19 @@ int main(int argc, string argv[])
 
     // Populate array of candidates
     candidate_count = argc - 1;
-    if (candidate_count > MAX)
+    if (candidate_count > MAX) //Checks to see if the numebr of candidates exceeds to allowed amount.
     {
         printf("Maximum number of candidates is %i\n", MAX);
         return 2;
     }
     for (int i = 0; i < candidate_count; i++)
     {
+        //The candidate's name was inputted by the user, so we can call their name with argv, the table of all the files
         candidates[i].name = argv[i + 1];
         candidates[i].votes = 0;
     }
 
-    int voter_count = get_int("Number of voters: ");
+    int voter_count = get_int("Number of voters: "); //This get the number of voters
 
     // Loop over all voters
     for (int m = 0; m < voter_count; m++)
@@ -65,12 +66,11 @@ int main(int argc, string argv[])
 
 // Update vote totals given a new vote
 int x = 0;
-bool vote(string name)
+bool vote(string name) //This is a function. that was previously called to check if the vote was valid.
 {
-    // TODO
-    for (int k = 0; k < candidate_count; k++)
+    for (int k = 0; k < candidate_count; k++) //Loops over all candidates
     {
-        if (strcmp(name, candidates[k].name) == 0)
+        if (strcmp(name, candidates[k].name) == 0) //Checks if there is a candidate by that name
         {
             candidates[k].votes++;
             return true;
@@ -83,16 +83,16 @@ int winnervotes;
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
     winner = candidates[0].name;
-    winnervotes = candidates[0].votes;
-    if (candidate_count > 1)
+    winnervotes = candidates[0].votes; //This assumes that the winner is he first candidate
+    
+    if (candidate_count > 1) //If there's more than 1 candidate...
     {
-        for (int n = 1; n < candidate_count; n++)
+        for (int n = 1; n < candidate_count; n++) //We loop over every candidate
         {
-            if (candidates[n].votes >= winnervotes)
+            if (candidates[n].votes >= winnervotes) //If a candidate gets more votes than the assumed "winner", they are declared the new one.
             {
-                winnervotes = candidates[n].votes;
+                winnervotes = candidates[n].votes; //Rather than keeping their name, we keep the votes.  That way, in case of a tie, we can tell who ALL the winners are.
             }
         }
         for (int p = 0; p < candidate_count; p++)
