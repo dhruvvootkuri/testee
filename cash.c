26ef2fd2ab2e5ffcc6c8c5a1e55df0029fd52e3b
Dhruv Fallon <50392973+kungfupandadab@users.noreply.github.com>
2021-01-27 17:28:39 -0800

    Update cash.c

--- cash.c
+++ cash.c
@@ -1,3 +1,8 @@
+//In this project, my assignment was to convert a certain amount of change, in dollars, into coins (quarters, dimes, nickels, and pennies).
+//However, I need to make it so that I use the least amount of coins as possible.
+//To do this, I made a system that keeps checking for the highest possible coin that fits in the change amount. 
+//Its value is then subtracted from the current change and the value that holds the number of coins increases by 1.
+
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
@@ -10,8 +15,8 @@ int main(void)
         dollars = get_float("Change owed:"); //gets change
     }
     while (dollars < 0 || dollars == 0); //conditional statements to prevent invalid change
-    int cents = round(dollars * 100);
-    int coins = 0;
+    int cents = round(dollars * 100); //The number of cents in the dollars
+    int coins = 0; //The number of coins it wil ltake to reach the number of cents
     do
     {
         if (cents > 25 || cents == 25) //if it is big enough to bite off a quarter
